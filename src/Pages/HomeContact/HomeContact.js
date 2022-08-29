f610a7f5b16ab3483b18d438deaf132526578d28
kdipto1 <kdipto@outlook.com>
2022-08-29 19:25:27 +0600

    Home contact section done

--- src/Pages/HomeContact/HomeContact.js
+++ src/Pages/HomeContact/HomeContact.js
@@ -1,19 +1,46 @@
 import React from "react";
+import contact from '../../Images/Login/contact.svg';
 
 const HomeContact = () => {
   return (
     <section className="mt-16 container mx-auto">
-      <h2 className="text-2xl font-bold text-center">Contact</h2>
+      {/* <h2 className="text-4xl font-bold text-center">Contact Us</h2> */}
       <div className="card lg:card-side bg-base-100 shadow-xl">
         <figure>
-          <img src="https://placeimg.com/400/400/arch" alt="Album" />
+          <img src={contact} alt="Album" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">New album is released!</h2>
-          <p>Click the button to listen on Spotiwhy app.</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Listen</button>
+          <div className="mx-auto">
+            <h2 className="card-title text-4xl ">Contact Us</h2>
           </div>
+          <form onClick={(e) => e.preventDefault()}>
+            <input
+              type="text"
+              placeholder="Your Name"
+              class="input input-bordered input-primary w-full max-w-xs"
+            />
+            <br />
+            <input
+              type="text"
+              placeholder="Your Email"
+              class="input input-bordered input-primary w-full max-w-xs my-4"
+            />
+            <br />
+            <textarea
+              cols={42}
+              rows={5}
+              class="textarea textarea-primary"
+              placeholder="Your Message"
+            ></textarea>
+            <br />
+            <div className="text-center">
+              <input
+                className="btn btn-wide mt-2 "
+                type="submit"
+                value="Send"
+              />
+            </div>
+          </form>
         </div>
       </div>
     </section>
